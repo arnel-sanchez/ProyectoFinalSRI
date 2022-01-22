d7fe1d4872b531020afe352d01318e18a59a5992
Samuel Efraín Pupo Wong <s.pupo@estudiantes.matcom.uh.cu>
2022-01-22 11:12:06 -0500

    Update Informe

--- Tester/draw.py
+++ Tester/draw.py
@@ -13,7 +13,7 @@ def average(numbers: [tuple]):
 def draw(points: [tuple]):
     points = sorted(points, key=lambda p: p[1])
     points = sorted(points, key=lambda p: p[0], reverse=True)
-    n = int(3 * len(points) / 4)
+    n = int(4 * len(points) / 5)
     points_avg = []
     for i in range(n, len(points)):
         points_avg.append(average(points[i-n:i]))
